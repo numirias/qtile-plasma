0accfe068bd38b71ab0ce770051800aaf64229e5
numirias <numirias@users.noreply.github.com>
2017-12-02 18:43:47 +0100

    Add setup file for PyPI

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='plasma',
+    packages=['plasma'],
+    version='0.1',
+    description='Plasma layout for Qtile',
+    author='numirias',
+    author_email='numirias+plasma@gmail.com',
+    url='https://github.com/numirias/qtile-plasma',
+    download_url='https://github.com/numirias/qtile-plasma/archive/v0.1.tar.gz',
+    keywords='qtile layout',
+    classifiers=[],
+)
