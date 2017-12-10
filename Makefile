d3a25132ee9e4fd20b087ca76d3fddb47a432aba
numirias <numirias@users.noreply.github.com>
2017-12-10 12:14:27 +0100

    Implement layout testing

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
 init:
 	pip install pipenv --upgrade
 	pipenv install --sequential --dev --skip-lock
+	cd lib/qtile/ && python ./libqtile/ffi_build.py
 test:
 	pipenv run pytest -v --cov plasma --cov-report term-missing:skip-covered tests/
 lint:
-	pipenv run flake8
+	pipenv run flake8 plasma/ tests/ 
 	pipenv run pylint --rcfile setup.cfg plasma/
 codecov:
 	pipenv run codecov
--- tests/test_plasma.py
+++ tests/test_plasma.py
@@ -1,58 +1,10 @@
-from pytest import fixture, approx
+from pytest import approx
 
 from plasma.debug import draw, tree, info
 from plasma.node import Node, VERTICAL, HORIZONTAL
 
+from .conftest import Nodes
 
-Node.min_size_default = 10
-
-@fixture
-def root():
-    root = Node('root', 0, 0, 120, 50)
-    return root
-
-@fixture
-def tiny_grid(root):
-    a, b, c = Nodes('a b c')
-    root.add_child(a)
-    root.add_child(b)
-    b.split_with(c)
-    return a, b, c
-
-@fixture
-def small_grid(root):
-    a, b, c, d = Nodes('a b c d')
-    root.add_child(a)
-    root.add_child(b)
-    b.split_with(c)
-    c.split_with(d)
-    return a, b, c, d
-
-@fixture
-def grid(root):
-    a, b, c, d, e = Nodes('a b c d e')
-    root.add_child(a)
-    root.add_child(b)
-    b.split_with(c)
-    c.split_with(d)
-    c.parent.add_child(e)
-    return a, b, c, d, e
-
-@fixture
-def complex_grid(root):
-    a, b, c, d, e, f, g = Nodes('a b c d e f g')
-    root.add_child(a)
-    root.add_child(b)
-    b.split_with(c)
-    c.split_with(d)
-    c.parent.add_child(e)
-    c.split_with(f)
-    f.split_with(g)
-    return a, b, c, d, e, f, g
-
-def Nodes(string):
-    for x in string.split():
-        yield Node(x)
 
 class TestPlasma:
 
@@ -613,10 +565,6 @@ class TestPlasma:
         assert a.width == 40
         a.height = 20
         assert a.height == 50
-
-    def test_grow_directions(self, root, grid):
-        a, b, c, d, e = grid
-        # TODO
 
     def test_min_size(self, root, small_grid):
         a, b, c, d = small_grid
