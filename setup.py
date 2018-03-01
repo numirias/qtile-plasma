afbe4e43fb659e33ba64f889ea35e925bd9af84a
numirias <numirias@users.noreply.github.com>
2018-03-01 23:19:09 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='qtile-plasma',
     packages=['plasma'],
-    version='1.5.1',
+    version='1.5.2',
     description='A flexible, tree-based layout for Qtile',
     author='numirias',
     author_email='numirias@users.noreply.github.com',
