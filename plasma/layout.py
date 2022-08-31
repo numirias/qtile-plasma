89c69655452735cc8d06fe3bc97e8ca4ecb1b1bc
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2022-09-01 02:44:27 +0300

    Implement `get_windows`

--- plasma/layout.py
+++ plasma/layout.py
@@ -60,6 +60,13 @@ class Plasma(Layout):
         clone.add_mode = None
         return clone
 
+    def get_windows(self):
+        clients = []
+        for leaf in self.root.all_leafs:
+            if leaf.payload is not None:
+                clients.append(leaf.payload)
+        return clients
+
     def add(self, client):
         node = self.root if self.focused_node is None else self.focused_node
         new = Node(client)
